d6289507ea39c13eea0c92b90a0e40effeec41c6
Abhidev ramesh <abhidevramesh@gmail.com>
2025-07-09 12:47:23 +0530

    added ui turn logic...

--- Assets/Scripts/TeamController.cs
+++ Assets/Scripts/TeamController.cs
@@ -1,14 +1,56 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TeamController : MonoBehaviour
 {
-    [SerializeField] GameObject[] TeamUI;
-    [SerializeField] int teamIndex;
-    private List<Button> buttons = new List<Button>();
-    void Awake()
+    [SerializeField] Button[] TeamUI;
+    [SerializeField] int teamIndex = 0;
+    [SerializeField] bool isDebug;
+
+    private int lastRollValue;
+
+    void OnEnable()
     {
-        // buttons = TeamUI.GetComponentsInChildren<Button>().ToList();
+        DiceScript.DiceRoll += HandleDiceRoll;
+    }
+    void OnDisable()
+    {
+        DiceScript.DiceRoll -= HandleDiceRoll;
+    }
+    void Start()
+    {
+        TurnIndication();
+    }
+
+    public void TurnIndication()
+    {
+        for (int i = 0; i < TeamUI.Length; i++)
+        {
+            TeamUI[i].enabled = (i == teamIndex);
+        }
+    }
+
+    public void HandleDiceRoll(int value)
+    {
+        Logger($" dice rolled : value is  {value}");
+        lastRollValue = value;
+        EndTurn();
+    }
+
+    private void EndTurn()
+    {
+        if (lastRollValue != 6)
+        {
+            teamIndex = (teamIndex + 1) % 4;
+        }
+        TurnIndication();
+    }
+
+    private void Logger(string message)
+    {
+        if (isDebug)
+        {
+            Debug.Log(message);
+        }
     }
 }
