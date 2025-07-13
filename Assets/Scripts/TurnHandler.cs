50ed7399702db222904e4a16c5e383931ff660cc
Abhidev ramesh <abhidevramesh@gmail.com>
2025-07-13 13:23:44 +0530

    added ui and team chance logic

---
+++ Assets/Scripts/TurnHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnHandler : MonoBehaviour
+{
+    private List<Transform> players = new List<Transform>();
+
+    public void ProcessTurn(int rollValue)
+    {
+
+    }
+}
